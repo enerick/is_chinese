b5c9848612be59bf3dcd0b87871f964c035db0f8
lidåshuång <ldshuang@gmail.com>
2016-03-02 20:39:00 +0800

    chore: publish to hex

--- mix.exs
+++ mix.exs
@@ -5,6 +5,8 @@ defmodule IsChinese.Mixfile do
     [app: :is_chinese,
      version: "1.0.0",
      elixir: "~> 1.2",
+     description: description,
+     package: package,
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      deps: deps]
@@ -15,6 +17,21 @@ defmodule IsChinese.Mixfile do
   # Type "mix help compile.app" for more information
   def application do
     [applications: [:logger]]
+  end
+
+  defp description do
+  """
+  Check whether string is Chinese
+  """
+  end
+
+  defp package do
+    [
+      files: ["lib", "mix.exs", "README*"],
+      maintainers: ["lidashuang"],
+      licenses: ["Apache 2.0"],
+      links: %{"GitHub" => "https://github.com/lidashuang/is_chinese"}
+    ]
   end
 
   # Dependencies can be Hex packages:
